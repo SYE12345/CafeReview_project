94d2586aea4a529042a0fac477bbacf4e2dc97ae
jinjoo <jinjuangel28@gmail.com>
2023-06-12 18:12:13 +0900

    예은

--- src/main/java/cafe/review/repository/cafe/CafeRepository.java
+++ src/main/java/cafe/review/repository/cafe/CafeRepository.java
@@ -1,10 +1,13 @@
 package cafe.review.repository.cafe;
 
+import cafe.review.domain.cafe.Cafe;
 import cafe.review.domain.cafe.CafeMember;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 @Slf4j
 @Repository
@@ -12,6 +15,10 @@ public class CafeRepository {
     private static final Map<Long, CafeMember> store = new HashMap<>();
 
     private static long sequence=0l;
+
+    public static List<CafeMember> findAll() {
+       return new ArrayList<>(store.values());
+    }
 
     public CafeMember save(CafeMember cafeMember){
         cafeMember.setCafeId(++sequence);
--- src/main/java/cafe/review/web/controller/cafe/CafeController.java
+++ src/main/java/cafe/review/web/controller/cafe/CafeController.java
@@ -1,8 +1,31 @@
 package cafe.review.web.controller.cafe;
 
+import cafe.review.domain.cafe.Cafe;
+import cafe.review.domain.cafe.CafeMember;
+import cafe.review.repository.cafe.CafeRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping(value = /)
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Controller
+@RequestMapping("cafe/All_list")
+@RequiredArgsConstructor
 public class CafeController {
+    private final CafeRepository cafeRepository;
+
+    @GetMapping
+    public String All_list(Model model){
+        List<CafeMember> cafeMembers = CafeRepository.findAll();
+        model.addAttribute("cafe",cafeMembers);
+        return "cafe/All_list";
+    }
 
 }
