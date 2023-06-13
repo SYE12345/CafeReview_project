a7bb16af29310361e7b9cb619cd3ea4151399e1c
SYE12345 <129352928+SYE12345@users.noreply.github.com>
2023-06-13 14:58:37 +0900

    sql

--- sql/Cafe_Member.sql
+++ sql/Cafe_Member.sql
@@ -1,13 +1,13 @@
 create table cafe_member
 (
     cafe_id number PRIMARY KEY,
-    cafe_name varchaar2(50),
+    cafe_name varchar2(50),
     address varchar2(50),
-    number varchar2(50),
+    cafe_number varchar2(50),
     menu1 varchar2(50),
     menu2 varchar2(50),
     menu3 varchar2(50),
-    type varchar2(50)
+    cafe_type varchar2(50)
 );
 
 create sequence cafe_member_seq
